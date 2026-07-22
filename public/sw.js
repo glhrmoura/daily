3273ec31963a0fdbcdb2744cce5005cc6f07f73d
Guilherme Moura <glhrmoura@gmail.com>
2026-07-21 22:17:46 -0300

    chore: bump service worker version to 21

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 20;
+const APP_VERSION = 21;
 const CACHE_NAME = `daily-app-v${APP_VERSION}-${Date.now()}`;
 const STATIC_CACHE = `daily-app-static-v${APP_VERSION}`;
 const DYNAMIC_CACHE = `daily-app-dynamic-v${APP_VERSION}`;
