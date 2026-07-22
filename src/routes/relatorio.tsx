04811b70cfcff9e196b1b791b7c86664a94a18fb
Guilherme Moura <glhrmoura@gmail.com>
2026-07-22 15:00:12 -0300

    feat: add relatorio route page

---
+++ src/routes/relatorio.tsx
@@ -0,0 +1,19 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { Header } from '@/components/Header';
+import { ReportPage } from '@/components/report';
+import { useTasks } from '@/hooks/useTasks';
+
+export const Route = createFileRoute('/relatorio')({
+  component: Relatorio,
+});
+
+function Relatorio() {
+  const { missed, hydrated } = useTasks();
+
+  return (
+    <div className="min-h-dvh bg-background">
+      <Header />
+      <ReportPage missed={missed} hydrated={hydrated} />
+    </div>
+  );
+}
