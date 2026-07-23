0e8f79ae27b658d9380794484cc69372d57b7a75
Guilherme Moura <glhrmoura@gmail.com>
2026-07-23 15:25:53 -0300

    chore: set pages dir and generated route-tree path

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: false,
+      routesDirectory: './src/pages',
+      generatedRouteTree: './src/routes/route-tree.gen.ts',
     }),
     react(),
     tailwindcss(),
