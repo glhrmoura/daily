d3ebcd56956c3a8ce9faf935fe99cf027b2c5b90
Guilherme Moura <glhrmoura@gmail.com>
2026-07-23 15:26:07 -0300

    refactor: import route tree from routes folder

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
 
 import i18n from '@/i18n';
-import { routeTree } from './routeTree.gen';
+import { routeTree } from './routes/route-tree.gen';
 import './styles.css';
 
 const queryClient = new QueryClient();
